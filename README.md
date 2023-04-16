b6f01126ea390786f9fd3301344adf524783d29c
Roxe322 <kolomatskiy@bk.ru>
2023-04-16 16:21:12 +0400

    Initial

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # POEWhisperTelegramNotifier
-Small script which checks your Client.txt log file and sending message to your telegram
+Small script which checks your Client.txt log file and sending message to your telegram when someone whispers you.
+
+## Requirements
+* [Python 3+](https://wiki.python.org/moin/BeginnersGuide/Download)
+
+## Setup
+Open `config.py` file with any text editor and change `LOG_PATH` and `TELEGRAM_BOT_TOKEN` and `TELEGRAM_CHAT_ID`. Don't remove quotes.
+
+## How to set config variables:
+* `LOG_PATH` - path to your `Client.txt` log file. You can find it in your `Path of Exile` folder. For example: `C:\Program Files (x86)\Grinding Gear Games\Path of Exile\logs\Client.txt` or `F:\Games\Steam\steamapps\common\Path of Exile\logs\client.txt`
+* `TELEGRAM_BOT_TOKEN` - you can get it from [@BotFather](https://t.me/BotFather). Just create new bot and copy token.
+* `TELEGRAM_CHAT_ID` - you can get it from [@userinfobot](https://t.me/userinfobot). Just send `/start` command and copy `id` field.
+
+
+## Run
+Run `python3 main.py` in terminal / cmd / powershell
---
+++ main.py
@@ -0,0 +1,76 @@
+import json
+import time
+from urllib import request
+
+from config import LOG_PATH, TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID
+
+API_URL = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage"
+
+
+def prepare_telegram_message(message):
+    """Remove unnecessary information from the message and apply formatting."""
+    split_message = message.split()
+    try:
+        if "[INFO Client" in message:
+            # from 2 to 7 there are debug log information that we don't want to see
+            del split_message[2:7]
+    except IndexError:
+        # Could be any other message, not only the whisper
+        pass
+    return " ".join(split_message).replace("@From", "From")  # Just to look better
+
+
+def follow(file):
+    file.seek(0, 2)
+    while True:
+        line = file.readline()
+        if not line:
+            time.sleep(0.1)
+            continue
+        yield line
+
+
+def make_data_to_send(message):
+    data = {"chat_id": TELEGRAM_CHAT_ID, "text": message}
+    json_data = json.dumps(data)
+
+    return json_data.encode("utf-8")
+
+
+def send_to_telegram(message):
+    """Send a message to a Telegram chat."""
+    message_text = prepare_telegram_message(message)
+    data = make_data_to_send(message_text)
+    req = request.Request(API_URL, data=data, method="POST")
+    print(API_URL)
+    req.add_header("Content-Type", "application/json")
+    print(data)
+    try:
+        with request.urlopen(req) as f:
+            print(f.read().decode("utf-8"))
+    except Exception as e:
+        print(e)
+
+
+if __name__ == "__main__":
+    send_to_telegram("Started to follow the log file for your whispers")
+    try:
+        with open(LOG_PATH, encoding="utf-8") as logfile:
+            log_lines = follow(logfile)
+            for log_line in log_lines:
+                print(log_line)
+                if "@From" in log_line:
+                    send_to_telegram(log_line)
+    except FileNotFoundError:
+        send_to_telegram(
+            "Log file not found at provided path, please check the path and restart the script"
+        )
+    except KeyboardInterrupt:
+        send_to_telegram(
+            "Stopped following the log file because of the keyboard interrupt, please restart the script"
+        )
+    except Exception as e:
+        print(e)
+        send_to_telegram(
+            f"Stopped following the log file because of the error: {e}, please restart the script"
+        )
